6f412edbfb8af244784c3d86135c2d8e4385d87f
Vincent Ninh DO <vincentnhdo@gmail.com>
2018-11-29 00:47:30 -0800

    Modify variable names in app/helpers/post_reminders.rb

--- app/helpers/post_reminders.rb
+++ app/helpers/post_reminders.rb
@@ -5,28 +5,28 @@ def post_reminders()
 
     # Say, the Reminder model has the only hash database like:
     #
-    # Reminder.channel_hash = {"@yawen":  ["@yawen", 30, "finish the README file",
+    # Reminder.user_hash = {"@yawen":  ["@yawen", 30, "finish the README file",
     #                          "@ivy": ["@myles", 3 * 24 * 60, "plan the timelines"}
     #
 
-    new_channel_hash = {}
+    new_user_hash = {}
 
-    Reminder.channel_hash.each do |key, value|
-        from_channel = key
-        to_channel = value[0]
+    Reminder.user_hash.each do |key, value|
+        from_user = key
+        to_user = value[0]
         duration = value[1] - 1
         action = value[2]
         if duration == 0
             # connect to Slack API
             # assume SlactAPI has method post taking two arguments:
-            #   the channel where it posts to: to_channel
+            #   the user where it posts to: to_user
             #   the message to be posted: message
-            SlackAPI.post(to_channel, "{from_channel} wanted you to know: {action}")
+            SlackAPI.post(to_user, "{from_channel} wanted you to know: {action}")
         else
-            new_channel_hash[from_channel] = [to_channel, duration, action]
+            new_user_hash[from_user] = [to_user, duration, action]
         end
     end
 
-    Reminder.channel_hash = new_channel_hash
+    Reminder.user_hash = new_user_hash
 
 end
